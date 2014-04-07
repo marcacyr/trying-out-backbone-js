8683623ffe1a8a73736171879cba59b63d55f0d8
Marc Cyr <marcacyr@gmail.com>
2014-04-07 10:27:05 -0700

    adding deleting

--- js/views/todos.js
+++ js/views/todos.js
@@ -26,13 +26,38 @@ app.TodoView = Backbone.View.extend({
 	// set a direct reference on the model for convenience.
 	initialize: function() {
 		this.listenTo(this.model, 'change', this.render);
+		this.listenTo(this.model, 'destroy', this.remove);
+		this.listenTo(this.model, 'visible', this.toggleVisible);
 	},
 
 	// Re-renders the titles of the todo item.
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
+
+		this.$el.toggleClass('completed', this.model.get('completed'));
+		this.toggleVisible();
+
 		this.$input = this.$('.edit');
 		return this;
+	},
+
+	// NEW - Toggles visibility of item
+	toggleVisible: function() {
+		this.$el.toggleClass('hidden', this.isHidden());
+	},
+
+	// NEW - Determines if item should be hidden
+	isHidden: function() {
+		var isCompleted = this.model.get('completed');
+		return (// hidden cases only
+			(!isCompleted && app.TodoFilter === 'completed')
+			|| (isCompleted && app.TodoFilter === 'active')
+		);
+	},
+
+	// NEW - Toggle the 'completed' state of the model.
+	toggleCompleted: function() {
+		this.model.toggle();
 	},
 
 	// Switch this view into '"editing"' mode, displaying the input field.
@@ -47,6 +72,8 @@ app.TodoView = Backbone.View.extend({
 
 		if (value) {
 			this.model.save({ title: value });
+		} else {
+			this.clear();
 		}
 
 		this.$el.removeClass('editing');
@@ -57,5 +84,10 @@ app.TodoView = Backbone.View.extend({
 		if (e.which === ENTER_KEY) {
 			this.close();
 		}
+	},
+
+	// NEW - Remove the item, destroy the model from localStorage and delete its view
+	clear: function() {
+		this.model.destroy();
 	}
 });
