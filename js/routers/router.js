78bcb850420931568a9c659f4addb20afb03cfb2
Marc Cyr <marcacyr@gmail.com>
2014-04-07 10:55:36 -0700

    remove documentation/comments from code

--- js/routers/router.js
+++ js/routers/router.js
@@ -1,22 +1,12 @@
-// js/routers/router.js
-
-// Todo router
-// -----------
-
 var Workspace = Backbone.Router.extend({
 	routes:{
 		'*filter': 'setFilter'
 	},
 
-	setFilter: function(param) {
-		// Set the current filter to be used
-		if (param) {
+	setFilter: function(param) {		if (param) {
 			param = param.trim();
 		}
 		app.TodoFilter = param || '';
-
-		// Trigger a collection filter event, causing hiding/unhiding
-		// of Todo view items
 		app.Todos.trigger('filter');
 	}
 });
--- js/views/app.js
+++ js/views/app.js
@@ -1,27 +1,16 @@
-// js/views/app.js
-
 var app = app || {};
 
-// The Application
-// ---------------
-
-// Our overall AppView is the top-level piece of the UI
 app.AppView = Backbone.View.extend({
-	// Instead of generating a new element, bing to the existing skeleton of the App already present in the HTML.
 	el: '#todoapp',
 
-	// Our template for the line of statistics at the bottom of the app.
 	statsTemplate: _.template( $('#stats-template').html()),
 
-	//Delegated events for creating new items, and clearing completed ones.
 	events: {
 		'keypress #new-todo': 'createOnEnter',
 		'click #clear-completed': 'clearCompleted',
 		'click #toggle-all': 'toggleAllComplete'
 	},	
 
-	// At initialization we bind to the relevant events on the 'Todos'
-	// collection, when items are added or changed.
 	initialize: function() {
 		this.allCheckbox = this.$('#toggle-all')[0];
 		this.$input = this.$('#new-todo');
@@ -38,8 +27,6 @@ app.AppView = Backbone.View.extend({
 		app.Todos.fetch();
 	},
 
-	// New
-	// Re-rendering the App just means refreshing the statistics -- the rest of the app does not change.
 	render: function() {
 		var completed = app.Todos.completed().length;
 		var remaining = app.Todos.remaining().length;
@@ -65,13 +52,11 @@ app.AppView = Backbone.View.extend({
 		this.allCheckbox.checked = !remaining;
 	},
 
-	// Add a single todo item to the list by creating a view for it, and appending its element to the <ul>
 	addOne: function(todo) {
 		var view = new app.TodoView({ model: todo });
 		$('#todo-list').append( view.render().el );
 	},
 
-	// Add all items in the Todos collection at once.
 	addAll: function() {
 		this.$('#todo-list').html('');
 		app.Todos.each(this.addOne, this);
@@ -85,7 +70,6 @@ app.AppView = Backbone.View.extend({
 		app.Todos.each(this.filterOne, this);
 	},
 
-	// Generate the attributes for a new Todo item.
 	newAttributes: function() {
 		return {
 			title: this.$input.val().trim(),
@@ -94,7 +78,6 @@ app.AppView = Backbone.View.extend({
 		};
 	},
 
-	// If you hit return in the main input field, create new Todo model, persisting it to localStorage.
 	createOnEnter: function(event) {
 		if (event.which !== ENTER_KEY || !this.$input.val().trim()) {
 			return;
